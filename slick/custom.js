d421cafd49d134a846326a42a7cf649d3ed9bbb8
KristaCalleja <kristacm@gmail.com>
2020-11-13 16:40:23 -0300

    Reframed for large desktops

--- slick/custom.js
+++ slick/custom.js
@@ -3,6 +3,13 @@ $(function (){
         infinite: true,
         slidesToShow: 2,
         slidesToScroll: 2,
-        arrows: true,
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0',
+        swipe: true,
+        swipeToSlide: true,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
     });
 });
