1cbc71d3f40548454a5479d4b29f9b38af808267
KristaCalleja <kristacm@gmail.com>
2020-12-14 18:20:01 -0300

    Debugging

--- slick/custom.js
+++ slick/custom.js
@@ -6,42 +6,14 @@ $(function (){
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 4,
+        swipe: false,
         arrows: false,
         centerMode: true,
         centerPadding: '0',
         pauseOnHover: false,
         pauseOnFocus: false,
-        swipe: true,
-        swipeToSlide: true,
-        variableWidth: true,
-        verticalSwiping: false,
         autoplay: true,
         autoplaySpeed: 1000,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 4,
-                    infinite: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    dots: false,
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    dots: false,
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
+        variableWidth: false,
     });
 });
