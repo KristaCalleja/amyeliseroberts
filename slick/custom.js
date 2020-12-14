91507980ee5bec68ad2b7bd37b753b8f46b71f13
KristaCalleja <kristacm@gmail.com>
2020-12-14 14:13:30 -0300

    Improved on design

--- slick/custom.js
+++ slick/custom.js
@@ -1,15 +1,47 @@
 $(function (){
     $('.multiple-items').slick({
+        adaptiveHeight: true,
+        dots: true,
+        dotsClass: 'slick-dots',
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 4,
         arrows: false,
         centerMode: true,
         centerPadding: '0',
+        pauseOnHover: false,
+        pauseOnFocus: false,
         swipe: true,
         swipeToSlide: true,
         variableWidth: true,
+        verticalSwiping: false,
         autoplay: true,
         autoplaySpeed: 1000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 4,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    dots: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    dots: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
