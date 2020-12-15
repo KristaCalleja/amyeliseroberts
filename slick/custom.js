d235cfa34c394246c73e0bea17e0bf842a4de8a3
KristaCalleja <kristacm@gmail.com>
2020-12-15 11:27:43 -0300

    Rearranged scripts

--- slick/custom.js
+++ slick/custom.js
@@ -1,19 +1,47 @@
 $(function (){
     $('.multiple-items').slick({
-        adaptiveHeight: true,
-        dots: true,
-        dotsClass: 'slick-dots',
+        adaptiveHeight: false,
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 4,
         swipe: false,
         arrows: false,
         centerMode: true,
-        centerPadding: '0',
+        centerPadding: '5',
         pauseOnHover: false,
         pauseOnFocus: false,
         autoplay: true,
         autoplaySpeed: 1000,
-        variableWidth: false,
+        verticalSwiping: false,        
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 4
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  unslicked: true,
+                  adaptiveHeight: false,
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  infinite: false,
+                  speed: 300
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  infinite: false,
+                  speed: 300
+                }
+              }
+        ]
     });
 });
